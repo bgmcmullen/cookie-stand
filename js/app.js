8cf126628a30f643ee5cb6cee9d4a4fa62f1a236
Brendan <bgmcmullen@comcast.net>
2023-11-14 11:54:34 -0500

    completed Lab-06

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+//get a window into HMTL file
 let citySection = document.getElementById('city-profiles');
 
 let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm', '7pm'];
 
+//create an object for each city
 let seattle = {
   name: 'Seattle',
   minCust: 23,
